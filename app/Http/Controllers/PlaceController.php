c5e972294d67e75760d0e58fb807239727f49b45
Thiago Correia <thiagogithub2002@gmail.com>
2023-01-23 02:09:30 -0300

    first commit

---
+++ app/Http/Controllers/PlaceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlaceController extends Controller
+{
+   // function public places(){
+     //   return view('Locais');
+    //}
+
+    public function hello(){
+        $name->'Joao'
+        return view('hello',compact('name'));
+    }
+}
